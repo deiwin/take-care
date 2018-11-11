bf2aa4d36e6083f7d201425174867ad49a6b3d75
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2018-11-11 20:44:40 +0200

    Abstract Slack error handling

--- src/Lib.hs
+++ src/Lib.hs
@@ -15,9 +15,10 @@ import System.FilePath (FilePath)
 import Data.Foldable (traverse_)
 import Data.Traversable (traverse)
 import Control.Monad ((>=>), mfilter)
-import Network.Wreq (defaults, param, header, getWith, postWith, asValue, responseBody, auth, oauth2Bearer)
+import Network.Wreq (defaults, param, header, getWith, postWith, asValue, responseBody, auth
+  , oauth2Bearer, Options, Response)
 import Data.Maybe (maybeToList, fromMaybe)
-import Data.Aeson (FromJSON, encode, toJSON, pairs, (.=), object)
+import Data.Aeson (FromJSON, encode, toJSON, pairs, (.=), object, Value)
 import Control.Lens ((&), (.~), (^?), (^?!), (^..), (^.), (?~))
 import Data.Aeson.Lens (key, values, _String, _Bool)
 import Data.List (find)
@@ -30,9 +31,6 @@ data InputRecord = InputRecord { members :: [Text]
                                , team :: Text -- TODO should be max 21 chars with the tm- prefix, so 18
                                , topic :: Text -> Text
                                } deriving (Generic, Show)
-
-u :: String -> String
-u = ("https://slack.com/api/" ++)
 
 instance Interpret InputRecord
 
@@ -58,31 +56,45 @@ createChannelID :: Text -> Token -> IO (Either Text Text)
 createChannelID name apiToken = do
     let opts = defaults & auth ?~ oauth2Bearer apiToken
     let body = toJSON $ object ["name" .= name]
-    resp <- asValue =<< postWith opts (u "conversations.create") body
+    resp <- asValue =<< postWith opts (slackURL "conversations.create") body
     -- TODO Should handle errors safely
     let Just id = trace (show $ resp ^? responseBody) $ resp ^? responseBody . key "channel" . key "id" . _String
     return $ Right id
+
+slackURL :: String -> String
+slackURL = ("https://slack.com/api/" ++)
+
+slackGet :: Token -> Options -> String -> IO (Either Text Value)
+slackGet apiToken opts method = do
+    let optsWithAuth = opts & auth ?~ oauth2Bearer apiToken
+    let url = slackURL method
+    resp <- asValue =<< getWith optsWithAuth url
+    let respBody = resp ^. responseBody
+    let ok = respBody ^?! key "ok" . _Bool
+    let errorMessage = respBody ^. key "error" . _String
+    let errorDetail = respBody ^? key "detail" . _String
+    if ok then
+        return $ Right respBody
+    else
+        return $ Left (errorMessage <> fromMaybe ""  errorDetail)
+
 
 findChannelID :: Text -> Token -> IO (Either Text (Maybe Text))
 findChannelID = findChannelID' Nothing
 findChannelID' :: Maybe Text -> Text -> Token -> IO (Either Text (Maybe Text))
 findChannelID' cursor name apiToken = do
-    let opts = defaults & auth ?~ oauth2Bearer apiToken
-                        & param "cursor" .~ maybeToList cursor
-    resp <- asValue =<< getWith opts (u "conversations.list")
-    let ok = resp ^?! responseBody . key "ok" . _Bool
-    let errorMessage = resp ^. responseBody . key "error" . _String
-    let errorDetail = resp ^? responseBody . key "detail" . _String
-    if not ok then
-        return $ Left (errorMessage <> fromMaybe ""  errorDetail)
-    else do
+    let opts = defaults & param "cursor" .~ maybeToList cursor
+    resp <- slackGet apiToken opts "conversations.list"
+    case resp of
+      Left err -> return $ Left err
+      Right respBody -> do
         let
-            channelID = (^.. values) <$> resp ^? responseBody . key "channels" >>=
+            channelID = (^.. values) <$> respBody ^? key "channels" >>=
                 find (\x -> (x ^? key "name". _String) == Just name) >>=
                     (^? key "id" . _String)
         let
             nextCursor = mfilter (not . T.null) $
-                resp ^? responseBody . key "response_metadata" . key "next_cursor" . _String
+                respBody ^? key "response_metadata" . key "next_cursor" . _String
         case channelID of
           Just id -> return $ Right $ Just id
           Nothing -> case nextCursor of

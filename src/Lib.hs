ce38f29b59199433e7256f14cb9dea77a9896f90
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2018-11-13 22:58:48 +0200

    Use monad transformers for error handling
    Already simplifies the current code by getting rid of some `case`s, but the
    real impact will show when `ensureTeamState` grows and has many steps. Handling
    these with `case` would get very tedious.

--- src/Lib.hs
+++ src/Lib.hs
@@ -24,6 +24,9 @@ import Control.Lens ((&), (.~), (^?), (^?!), (^..), (^.), (?~), _Just)
 import Data.Aeson.Lens (key, values, _String, _Bool)
 import Data.List (find, intercalate)
 import Data.Map as Map (fromList)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Except (ExceptT, runExceptT)
+import Control.Error.Util (hoistEither)
 
 import Debug.Trace (trace)
 
@@ -42,59 +45,60 @@ someFunc :: FilePath -> Token -> IO ()
 someFunc inputPath apiToken = do
     input <- readInput inputPath
     traverse_ print input
-    traverse_ (ensureTeamState apiToken) input
+    -- TODO log error on failures (left) instead of simply ignoring the
+    -- results
+    traverse_ (runExceptT . ensureTeamState apiToken) input
 
-ensureTeamState :: Token -> InputRecord -> IO ()
+ensureTeamState :: Token -> InputRecord -> ExceptT Text IO ()
 ensureTeamState apiToken record = do
     let channelName = "tm-" <> team record
     channelID <- findOrCreateChannelID apiToken channelName (members record)
-    print channelID
+    lift $ print channelID
 
-findOrCreateChannelID :: Token -> Text -> [Text] -> IO (Either Text Text)
+findOrCreateChannelID :: Token -> Text -> [Text] -> ExceptT Text IO Text
 findOrCreateChannelID apiToken name userIDs = do
-    currentIDResult <- findChannelID name apiToken
-    case currentIDResult of
-      Left err -> return $ Left err
-      Right currentID -> maybe (createChannelID apiToken name userIDs) (return . Right) currentID
+    currentID <- findChannelID name apiToken
+    maybe (createChannelID apiToken name userIDs) return currentID
 
-createChannelID :: Token -> Text -> [Text] -> IO (Either Text Text)
+createChannelID :: Token -> Text -> [Text] -> ExceptT Text IO Text
 createChannelID apiToken name userIDs = do
     let params = [ "name" .= name
                  , "user_ids" .= intercalate "," (T.unpack <$> userIDs)
                  ]
-    resp <- slackPost apiToken params "conversations.create"
-    return $ (^. key "channel" . key "id" . _String) <$> resp
+    respBody <- slackPost apiToken params "conversations.create"
+    return $ respBody ^. key "channel" . key "id" . _String
 
 slackURL :: String -> String
 slackURL = ("https://slack.com/api/" ++)
 
-slackGet :: Token -> Options -> String -> IO (Either Text Value)
-slackGet apiToken opts method = handleSlackError "GET" method <$> (asValue =<< getWith optsWithAuth url)
-    where optsWithAuth = opts & auth ?~ oauth2Bearer apiToken
-          url = slackURL method
+slackGet :: Token -> Options -> String -> ExceptT Text IO Value
+slackGet apiToken opts method = do
+    let optsWithAuth = opts & auth ?~ oauth2Bearer apiToken
+    let url = slackURL method
+    resp <- lift (asValue =<< getWith optsWithAuth url)
+    hoistEither $ handleSlackError "GET" method resp
 
-slackGetPaginated :: Token -> Options -> String -> IO (Either Text [Value])
+slackGetPaginated :: Token -> Options -> String -> ExceptT Text IO [Value]
 slackGetPaginated = slackGetPaginated' Nothing []
-slackGetPaginated' :: Maybe Text -> [Value] -> Token -> Options -> String -> IO (Either Text [Value])
+slackGetPaginated' :: Maybe Text -> [Value] -> Token -> Options -> String -> ExceptT Text IO [Value]
 slackGetPaginated' cursor acc apiToken opts method = do
     let optsWithCursor = defaults & param "cursor" .~ maybeToList cursor
-    resp <- slackGet apiToken optsWithCursor "conversations.list"
-    case resp of
-      Left err -> return $ Left err
-      Right respBody -> do
-          let
-            nextCursor = mfilter (not . T.null) $
-                respBody ^? key "response_metadata" . key "next_cursor" . _String
-          let nextAcc = respBody : acc
-          case nextCursor of
-            Just c -> slackGetPaginated' nextCursor nextAcc apiToken opts method
-            Nothing -> return $ Right $ reverse nextAcc
+    respBody <- slackGet apiToken optsWithCursor "conversations.list"
+    let
+      nextCursor = mfilter (not . T.null) $
+          respBody ^? key "response_metadata" . key "next_cursor" . _String
+    let nextAcc = respBody : acc
+    case nextCursor of
+      Just c -> slackGetPaginated' nextCursor nextAcc apiToken opts method
+      Nothing -> return $ reverse nextAcc
 
-slackPost :: Token -> [Pair] -> String -> IO (Either Text Value)
-slackPost apiToken params method = handleSlackError "POST" method <$> (asValue =<< postWith optsWithAuth url body)
-    where optsWithAuth = defaults & auth ?~ oauth2Bearer apiToken
-          url = slackURL method
-          body = toJSON $ object params
+slackPost :: Token -> [Pair] -> String -> ExceptT Text IO Value
+slackPost apiToken params method = do
+    let optsWithAuth = defaults & auth ?~ oauth2Bearer apiToken
+    let url = slackURL method
+    let body = toJSON $ object params
+    resp <- lift $ asValue =<< postWith optsWithAuth url body
+    hoistEither $ handleSlackError "POST" method resp
 
 handleSlackError :: Text -> String -> Response Value -> Either Text Value
 handleSlackError httpMethod method resp =
@@ -108,14 +112,11 @@ handleSlackError httpMethod method resp =
             Left (httpMethod <> " " <> T.pack method <> ": " <> error <> maybe "" (" - " <>) detail)
 
 
-findChannelID :: Text -> Token -> IO (Either Text (Maybe Text))
+findChannelID :: Text -> Token -> ExceptT Text IO (Maybe Text)
 findChannelID name apiToken = do
-    resp <- slackGetPaginated apiToken defaults "conversations.list"
-    case resp of
-      Left err -> return $ Left err
-      Right respBodies -> do
-          let
-              channel = find (\x -> (x ^? key "name". _String) == Just name) $
-                  respBodies ^.. traverse . key "channels" . values
-          let channelID = channel ^? _Just . key "id" . _String
-          return $ Right channelID
+    respBodies <- slackGetPaginated apiToken defaults "conversations.list"
+    let
+        channel = find (\x -> (x ^? key "name". _String) == Just name) $
+            respBodies ^.. traverse . key "channels" . values
+    let channelID = channel ^? _Just . key "id" . _String
+    return channelID

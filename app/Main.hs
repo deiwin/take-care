bd4851aac87ddacd930bf996989587a3d07be1f5
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2018-11-17 11:54:40 +0200

    Improve error message on missing env variable

--- app/Main.hs
+++ app/Main.hs
@@ -7,9 +7,10 @@ import System.Environment (lookupEnv)
 import Data.ByteString.Char8 as BS (pack)
 import Prelude hiding (getContents)
 import Data.Text.IO (getContents)
+import Data.Maybe (fromMaybe)
 
 main :: IO ()
 main = do
     inputText <- getContents
-    Just apiToken <- lookupEnv "API_TOKEN"
+    apiToken <- fromMaybe (error "API_TOKEN env variable not set") <$> lookupEnv "API_TOKEN"
     ensure inputText (BS.pack apiToken)
